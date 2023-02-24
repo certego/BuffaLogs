4eef14a53a7c894fde2c61bc3e8935058726ae59
Lorygold <lorygold10@gmail.com>
2023-02-24 15:41:57 +0100

    Update Elasticsearch template

--- config/elasticsearch/load_templates.sh
+++ config/elasticsearch/load_templates.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-curl -X PUT "localhost:9200/_template/ecs?pretty" -H 'Content-Type: application/json' -d'@./ecs.json' 
-curl -X PUT "localhost:9200/_template/wfd?pretty" -H 'Content-Type: application/json' -d'@./wfd.json' 
+curl -X PUT "localhost:9200/_template/example?pretty" -H 'Content-Type: application/json' -d'@./example_template.json'
