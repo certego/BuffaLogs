2e2d1a5c7bcfae749990daeef886384f7a47a1c6
lorygold <lorygold10@gmail.com>
2023-09-07 12:27:24 +0200

    Added ignored_users and ignored_ips into Config model

--- buffalogs/impossible_travel/admin.py
+++ buffalogs/impossible_travel/admin.py
@@ -37,5 +37,5 @@ class TaskSettingsAdmin(admin.ModelAdmin):
 
 @admin.register(Config)
 class ConfigsAdmin(admin.ModelAdmin):
-    list_display = ("created", "updated", "allowed_countries", "vip_users")
+    list_display = ("created", "updated", "ignored_users", "ignored_ips", "allowed_countries", "vip_users")
     search_fields = ("allowed_countries", "vip_users")
