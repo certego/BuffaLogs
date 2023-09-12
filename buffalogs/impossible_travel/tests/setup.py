5d6ffaf2e3abab9c53e1d7605894417916c17852
lorygold <lorygold10@gmail.com>
2023-09-12 08:51:39 +0200

    Improved tests

--- buffalogs/impossible_travel/tests/setup.py
+++ buffalogs/impossible_travel/tests/setup.py
@@ -45,7 +45,7 @@ class Setup:
         )
         Config.objects.create(
             ignored_users=["N/A", "Not Available"],
-            ignored_ips=["0.0.0.0", "0.0.0.0/0"],
+            ignored_ips=["0.0.0.0", "192.168.1.0/24"],
             allowed_countries=["Italy", "United States"],
             vip_users=["Asa Strickland", "Krista Moran"],
         )
