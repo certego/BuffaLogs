cb3e46f1bf9498f4520ec6ec8bbdbef122835224
lorygold <lorygold10@gmail.com>
2023-09-19 16:32:00 +0200

    Implemented Certego shared CI 1.3.5

--- .github/.github/test/python_test/source.py
+++
@@ -1,8 +0,0 @@
-class MyTestClass:
-    def __init__(self):
-        self.first_element = 1
-        self.second_element = 2
-        self.third_element = 3
-
-    def my_first_element(self):
-        return self.first_element
