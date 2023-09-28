5d66f65da5fdea89ccfc4a5113165e6f69600f7b
Lorena Goldoni <33703137+Lorygold@users.noreply.github.com>
2023-09-28 17:33:28 +0200

    1.2.2 (#71)
    * Setup config (#70)
    
    * Updated setup_config mgmt command
    
    * Updated CHANGELOG.md
    
    * Added cover
    
    * Update README.md
    
    * Managed values as a list
    
    * Refactoring
    
    * Removed comments
    
    * buffalogs_1_2_2

--- buffalogs/impossible_travel/management/commands/setup_config.py
+++ buffalogs/impossible_travel/management/commands/setup_config.py
@@ -5,42 +5,41 @@ from impossible_travel.models import Config
 
 logger = logging.getLogger()
 IGNORED_USERS = ["N/A", "Not Available"]
-IGNORED_IPS = [""]
+IGNORED_IPS = ["127.0.0.1"]
 
 
 class Command(BaseCommand):
-    help = "Command to setup configs in the Config model"
+    help = "Setup the Configs overwriting them"
 
     def add_arguments(self, parser):
         # Optional arguments
-        parser.add_argument("--ignored_users", nargs="?", action="append", default=[], help="List of users to filter and to not consider in the detection")
-        parser.add_argument("--ignored_ips", nargs="?", action="append", default=[], help="List of ips or subnets to not consider in the detection")
-        parser.add_argument("--allowed_countries", nargs="?", action="append", default=[], help="List of countries from which the logins are always allowed")
-        parser.add_argument("--vip_users", nargs="?", action="append", default=[], help="List of users to which pay particular attention")
+        parser.add_argument("--ignored_users", nargs="?", default=[], help="List of users to filter and to not consider in the detection")
+        parser.add_argument("--ignored_ips", nargs="?", default=[], help="List of ips or subnets to not consider in the detection")
+        parser.add_argument("--allowed_countries", nargs="?", default=[], help="List of countries from which the logins are always allowed")
+        parser.add_argument("--vip_users", nargs="?", default=[], help="List of users to which pay particular attention")
 
     def handle(self, *args, **options):
         """Setup the configurations into the Config model"""
         logger = logging.getLogger()
-        print(options)
-        if not options["ignored_users"] and not options["ignored_ips"] and not options["allowed_countries"] and not options["vip_users"]:
-            # Setup default configs
-            Config.objects.update_or_create(ignored_users=IGNORED_USERS, ignored_ips=IGNORED_IPS)
+
+        if Config.objects.all().exists():
+            config_obj = Config.objects.all()[0]
         else:
-            if not Config.objects.exists():
-                Config.objects.create(
-                    ignored_users=options["ignored_users"],
-                    ignored_ips=options["ignored_ips"],
-                    allowed_countries=options["allowed_countries"],
-                    vip_users=["vip_users"],
-                )
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Configs set correctly:\
-                                                    \nIgnored users: {options['ignored_users']}\
-                                                    \nIgnored ips: {options['ignored_ips']}\
-                                                    \nAllowed countries: {options['allowed_countries']}\
-                                                    \nVip users: {options['vip_users']}"
-                    )
-                )
-            else:
-                self.stdout.write(self.style.ERROR("Error: Configs already exist. Use the update_config or the clear_models commands"))
+            config_obj = Config.objects.create()
+        if not options["ignored_users"] and not options["ignored_ips"] and not options["allowed_countries"] and not options["vip_users"]:
+            # Set default values
+            config_obj.ignored_users = IGNORED_USERS
+            config_obj.ignored_ips = IGNORED_IPS
+        else:
+            for opt in options:
+                if opt in ["ignored_users", "ignored_ips", "allowed_countries", "vip_users"]:
+                    if not options[opt]:
+                        setattr(config_obj, opt, [])
+                    else:
+                        setattr(config_obj, opt, options[opt].split(","))
+
+        config_obj.save()
+
+        logger.info(
+            f"Updated Config values - Ignored users: {config_obj.ignored_users}, Ignored IPs: {config_obj.ignored_ips}, Allowed countries: {config_obj.allowed_countries}, Vip users: {config_obj.vip_users}"
+        )
