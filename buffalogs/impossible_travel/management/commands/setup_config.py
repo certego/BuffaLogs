ee783d686d983888432294cf86f3141fc70b9add
lorygold <lorygold10@gmail.com>
2023-09-28 15:21:32 +0200

    Managed values as a list

--- buffalogs/impossible_travel/management/commands/setup_config.py
+++ buffalogs/impossible_travel/management/commands/setup_config.py
@@ -13,10 +13,10 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         # Optional arguments
-        parser.add_argument("--ignored_users", nargs="?", action="append", default=[], help="List of users to filter and to not consider in the detection")
-        parser.add_argument("--ignored_ips", nargs="?", action="append", default=[], help="List of ips or subnets to not consider in the detection")
-        parser.add_argument("--allowed_countries", nargs="?", action="append", default=[], help="List of countries from which the logins are always allowed")
-        parser.add_argument("--vip_users", nargs="?", action="append", default=[], help="List of users to which pay particular attention")
+        parser.add_argument("--ignored_users", nargs="?", default=[], help="List of users to filter and to not consider in the detection")
+        parser.add_argument("--ignored_ips", nargs="?", default=[], help="List of ips or subnets to not consider in the detection")
+        parser.add_argument("--allowed_countries", nargs="?", default=[], help="List of countries from which the logins are always allowed")
+        parser.add_argument("--vip_users", nargs="?", default=[], help="List of users to which pay particular attention")
 
     def handle(self, *args, **options):
         """Setup the configurations into the Config model"""
@@ -28,31 +28,22 @@ class Command(BaseCommand):
                 config_obj.ignored_users = IGNORED_USERS
                 config_obj.ignored_ips = IGNORED_IPS
             else:
-                config_obj.ignored_users = options["ignored_users"]
-                config_obj.ignored_ips = options["ignored_ips"]
-                config_obj.allowed_countries = options["allowed_countries"]
-                config_obj.vip_users = options["vip_users"]
-            config_obj.save()
+                config_obj.ignored_users = options["ignored_users"].split(",")
+                # config_obj.ignored_ips = options["ignored_ips"].split(",")
+                # config_obj.allowed_countries = options["allowed_countries"].split(",")
+                # config_obj.vip_users = options["vip_users"].split(",")
         else:
             if not options["ignored_users"] and not options["ignored_ips"] and not options["allowed_countries"] and not options["vip_users"]:
-                Config.objects.create(ignored_users=IGNORED_USERS, ignored_ips=IGNORED_IPS)
+                config_obj = Config.objects.create(ignored_users=IGNORED_USERS, ignored_ips=IGNORED_IPS)
             else:
-                Config.objects.create(
-                    ignored_users=options["ignored_users"],
-                    ignored_ips=options["ignored_ips"],
-                    allowed_countries=options["allowed_countries"],
-                    vip_users=options["vip_users"],
+                config_obj = Config.objects.create(
+                    ignored_users=options["ignored_users"].split(","),
+                    # ignored_ips=options["ignored_ips"].split(","),
+                    # allowed_countries=options["allowed_countries"].split(","),
+                    # vip_users=options["vip_users"].split(","),
                 )
+        config_obj.save()
 
-        count_config = Config.objects.all().count()
-        config = Config.objects.all()[0]
-        print(f"COUNT CONFIGS:{count_config}")
-        self.stdout.write(
-            self.style.SUCCESS(
-                f"Configs set correctly:\
-                        \nIgnored users: {config.ignored_users}\
-                        \nIgnored ips: {config.ignored_ips}\
-                        \nAllowed countries: {config.allowed_countries}\
-                        \nVip users: {config.vip_users}"
-            )
+        logger.info(
+            f"Updated Config values - Ignored users: {config_obj.ignored_users}, Ignored IPs: {config_obj.ignored_ips}, Allowed countries: {config_obj.allowed_countries}, Vip users: {config_obj.vip_users}"
         )
