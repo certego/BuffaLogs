f7da3f036e2e133897adb91257d5f47f67ec9fa2
Lorena Goldoni <33703137+Lorygold@users.noreply.github.com>
2024-03-07 13:00:02 +0100

    Fixed login.updated field (#99)
    * Fixed the update of the login.updated field
    
    * Added logging to the clear_models_periodically function
    
    * Added tests on login.updated field
    
    * Updated CHANGELOG.md
    
    * Updated check on login.updated field

--- buffalogs/impossible_travel/tasks.py
+++ buffalogs/impossible_travel/tasks.py
@@ -18,16 +18,20 @@ def clear_models_periodically():
     app_config = Config.objects.get(id=1)
     now = timezone.now()
     delete_user_time = now - timedelta(days=app_config.user_max_days)
-    User.objects.filter(updated__lte=delete_user_time).delete()
+    deleted_users, _ = User.objects.filter(updated__lte=delete_user_time).delete()
+    logger.info(f"Deleted {deleted_users} users for periodic cleaning")
 
     delete_login_time = now - timedelta(days=app_config.login_max_days)
-    Login.objects.filter(updated__lte=delete_login_time).delete()
+    deleted_logins, _ = Login.objects.filter(updated__lte=delete_login_time).delete()
+    logger.info(f"Deleted {deleted_logins} logins for periodic cleaning")
 
     delete_alert_time = now - timedelta(days=app_config.alert_max_days)
-    Alert.objects.filter(updated__lte=delete_alert_time).delete()
+    deleted_alerts, _ = Alert.objects.filter(updated__lte=delete_alert_time).delete()
+    logger.info(f"Deleted {deleted_alerts} alerts for periodic cleaning")
 
     delete_ip_time = now - timedelta(days=app_config.ip_max_days)
-    UsersIP.objects.filter(updated__lte=delete_ip_time).delete()
+    deleted_usersIP, _ = UsersIP.objects.filter(updated__lte=delete_ip_time).delete()
+    logger.info(f"Deleted {deleted_usersIP} usersIP for periodic cleaning")
 
 
 @shared_task(name="BuffalogsUpdateRiskLevelTask")
