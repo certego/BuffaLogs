fbdce0d4eb06b4dd0ee7e4e23799df89d91d9a27
drona-gyawali <dronarajgyawali@gmail.com>
2025-03-11 18:29:46 +0545

    fix/gui-widget

--- buffalogs/impossible_travel/static/js/homepage.js
+++ buffalogs/impossible_travel/static/js/homepage.js
@@ -21,28 +21,30 @@ function post(path, params, method='post') {
     const form = document.createElement('form');
     form.method = method;
     form.action = path;
-  
+
     for (const key in params) {
-      if (params.hasOwnProperty(key)) {
-        const hiddenField = document.createElement('input');
-        hiddenField.type = 'hidden';
-        hiddenField.name = key;
-        hiddenField.value = params[key];
-  
-        form.appendChild(hiddenField);
-      }
+        if (params.hasOwnProperty(key)) {
+            const hiddenField = document.createElement('input');
+            hiddenField.type = 'hidden';
+            hiddenField.name = key;
+            hiddenField.value = params[key];
+
+            form.appendChild(hiddenField);
+        }
     }
-  
+
     document.body.appendChild(form);
     form.submit();
-  }
+}
 
+// This is Global variable to store grid options for access in other functions
+let gridOptions;
 
 // live alerts chart
 document.addEventListener( "DOMContentLoaded", function () {
 
     const columns = [
-        { 
+        {
             field: "user",
             headerName: "Username",
             resizable: true,
@@ -50,8 +52,10 @@ document.addEventListener( "DOMContentLoaded", function () {
             sortable: true,
             unSortIcon: true,
             filter: true,
-        }, 
-        { 
+            flex: 1,
+            minWidth: 100
+        },
+        {
             field: "timestamp",
             headerName: "Timestamp",
             resizable: true,
@@ -59,28 +63,37 @@ document.addEventListener( "DOMContentLoaded", function () {
             unSortIcon: true,
             editable: true,
             filter: true,
-        }, 
-        { 
+            flex: 1,
+            minWidth: 120
+        },
+        {
             field: "name",
             headerName: "Alert name",
             resizable: true,
             editable: true,
-            sortable:  true,
+            sortable: true,
             unSortIcon: true,
             filter: true,
-        }, 
-    ]
+            flex: 1,
+            minWidth: 100
+        }
+    ];
 
     function fetch_request() {
-        fetch("/get_last_alerts", {method: 'GET', headers: {"Accept":"application/json", 
-            "X-Requested-With":"XMLHttpRequest"}})
-        // gestisci il successo
-        .then(response => response.json())  // converti a json
-        .then((data) => {
-            gridOptions.rowData = (JSON.parse(data));
-            gridOptions.api.setRowData(JSON.parse(data));
-        })  
-        .catch(err => console.log('Request Failed', err)); // gestisci gli errori
+        fetch("/get_last_alerts", {
+            method: 'GET',
+            headers: {
+                "Accept": "application/json",
+                "X-Requested-With": "XMLHttpRequest"
+            }
+        })
+            .then(response => response.json())
+            .then((data) => {
+                const parsedData = JSON.parse(data);
+                gridOptions.rowData = parsedData;
+                gridOptions.api.setRowData(parsedData);
+            })
+            .catch(err => console.log('Request Failed', err));
     }
 
     // Grid options (to customize grid)
@@ -98,13 +111,14 @@ document.addEventListener( "DOMContentLoaded", function () {
         undoRedoCellEditingLimit: 10,
         cacheQuickFilter: true,
 
-        suppressMultiSort:true,
-        accentedSort:true,
+        suppressMultiSort: true,
+        accentedSort: true,
 
-        defaultColDef:{
+        defaultColDef: {
             resizable: false,
-            editable:false,
+            editable: false,
             filter: false,
+            flex: 1
         },
 
         //Pagination options
@@ -119,54 +133,76 @@ document.addEventListener( "DOMContentLoaded", function () {
         suppressCellSelection: true,
 
         enableBrowserTooltips: true,
-        
+
         onGridReady: event => {
             fetch_request();
+
+            // Handle grid size initially and when parent container resizes
+            const resizeObserver = new ResizeObserver(() => {
+                setTimeout(() => {
+                    event.api.sizeColumnsToFit();
+                }, 100);
+            });
+
+            const gridContainer = document.querySelector('.live-alerts-container');
+            if (gridContainer) {
+                resizeObserver.observe(gridContainer);
+            }
         }
     };
 
     // get div to host the grid
     const eGridDiv = document.getElementById("myGrid");
-    // new grid instance, passing in the hosting DIV and Grid Options
-    new agGrid.Grid(eGridDiv, gridOptions);  
 
-    gridOptions.api.sizeColumnsToFit();
+    // Only create the grid if the element exists
+    if (eGridDiv) {
+        // new grid instance, passing in the hosting DIV and Grid Options
+        new agGrid.Grid(eGridDiv, gridOptions);
 
+        gridOptions.api.sizeColumnsToFit();
+
+    }
 })
 
 function cb(start, end) {
     $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
 }
 
-var end = moment(new Date($('#reportrange').attr("data-enddate")));
-var start = moment(new Date($('#reportrange').attr("data-startdate")));
+// Initialize date range picker
+document.addEventListener("DOMContentLoaded", function () {
+    const reportRangeElement = document.getElementById('reportrange');
+    if (reportRangeElement) {
+        var end = moment(new Date(reportRangeElement.getAttribute("data-enddate")));
+        var start = moment(new Date(reportRangeElement.getAttribute("data-startdate")));
 
-$('#reportrange').daterangepicker({
-    startDate: start,
-    endDate: end,
-    maxDate: new Date(),
-    ranges: {
-       'Today': [moment().utc().set({h:0, m:0, s:0}), moment().utc()],
-       'Yesterday': [moment().utc().subtract(1, 'days').set({h:0, m:0, s:0}), moment().utc().subtract(1, 'days').set({h:23, m:59, s:59})],
-       'Last 7 Days': [moment().utc().subtract(6, 'days'), moment().utc()],
-       'Last 30 Days': [moment().utc().subtract(29, 'days'), moment().utc()],
-       'This Month': [moment().utc().startOf('month'), moment().utc().endOf('month')],
-       'Last Month': [moment().utc().subtract(1, 'month').startOf('month'), moment().utc().subtract(1, 'month').endOf('month')]
-    }
-}, cb);
-cb(start, end);
+        $('#reportrange').daterangepicker({
+            startDate: start,
+            endDate: end,
+            maxDate: new Date(),
+            ranges: {
+                'Today': [moment().utc().set({h:0, m:0, s:0}), moment().utc()],
+                'Yesterday': [moment().utc().subtract(1, 'days').set({h:0, m:0, s:0}), moment().utc().subtract(1, 'days').set({h:23, m:59, s:59})],
+                'Last 7 Days': [moment().utc().subtract(6, 'days'), moment().utc()],
+                'Last 30 Days': [moment().utc().subtract(29, 'days'), moment().utc()],
+                'This Month': [moment().utc().startOf('month'), moment().utc().endOf('month')],
+                'Last Month': [moment().utc().subtract(1, 'month').startOf('month'), moment().utc().subtract(1, 'month').endOf('month')]
+            }
+        }, cb);
+        cb(start, end);
 
 
 $('#reportrange').on('apply.daterangepicker', function() {
-    let start = $('#reportrange').data('daterangepicker').startDate.toDate();
-    let end = $('#reportrange').data('daterangepicker').endDate.toDate();
-    let date_range = parseRangeData(start, end);
-    cb(date_range[0], date_range[1]);
+            let start = $('#reportrange').data('daterangepicker').startDate.toDate();
+            let end = $('#reportrange').data('daterangepicker').endDate.toDate();
+            let date_range = parseRangeData(start, end);
+            cb(date_range[0], date_range[1]);
 
-    // POST
-    const csrftoken = getCookie('csrftoken');
-    let param = {"csrfmiddlewaretoken": csrftoken, "date_range": JSON.stringify(date_range)}
-    post(window.location.origin+'/homepage/', param)
+            // POST
+            const csrftoken = getCookie('csrftoken');
+            let param = { "csrfmiddlewaretoken": csrftoken, "date_range": JSON.stringify(date_range) };
+            post(window.location.origin + '/homepage/', param);
+        });
+    }
 });
 
 
@@ -175,7 +211,7 @@ function parseRangeData(start_date, end_date){
     var end_tosend = moment(new Date(end_date));
     days_diff_datetime = end_tosend.diff(start_tosend, 'days');
     const date_range = [];
-    date_range[0] = start_tosend; 
+    date_range[0] = start_tosend;
     date_range[1] = end_tosend;
     return date_range
 }
