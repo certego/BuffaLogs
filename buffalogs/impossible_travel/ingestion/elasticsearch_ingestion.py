aa726ebf0c1ebd97b87b3d7d02b248b7238d4cc7
Lorygold <lory.goldoni@gmail.com>
2025-03-21 17:52:00 +0100

    Refactored the ElasticsearchIngestion.normalize_fields

--- buffalogs/impossible_travel/ingestion/elasticsearch_ingestion.py
+++ buffalogs/impossible_travel/ingestion/elasticsearch_ingestion.py
@@ -1,10 +1,8 @@
 import logging
+from datetime import datetime
 
-from django.conf import settings
 from elasticsearch_dsl import Search, connections
 from impossible_travel.ingestion.base_ingestion import BaseIngestion
-from impossible_travel.models import User
-from impossible_travel.modules import detection
 
 
 class ElasticsearchIngestion(BaseIngestion):
@@ -13,13 +11,27 @@ class ElasticsearchIngestion(BaseIngestion):
     """
 
     def __init__(self, ingestion_config: dict):
+        """
+        Constructor for the Elasticsearch Ingestion object
+        """
         super().__init__()
         self.elastic_config = ingestion_config
         # create the elasticsearch host connection
         connections.create_connection(hosts=self.elastic_config["url"], timeout=self.elastic_config["timeout"], verify_certs=False)
         self.logger = logging.getLogger(f"{__name__}.{self.__class__.__name__}")
 
-    def process_users(self, start_date, end_date) -> list:
+    def process_users(self, start_date: datetime, end_date: datetime) -> list:
+        """
+        Concrete implementation of the BaseIngestion.process_users abstract method
+
+        :param start_date: the initial datetime from which the users are considered
+        :type start_date: datetime (with tzinfo=datetime.timezone.utc)
+        :param end_date: the final datetime within which the users are considered
+        :type end_date: datetime (with tzinfo=datetime.timezone.utc)
+
+        :return: list of users strings that logged in Elasticsearch
+        :rtype: list
+        """
         self.logger.info(f"Starting at: {start_date} Finishing at: {end_date}")
         users_list = []
         s = (
@@ -40,16 +52,32 @@ class ElasticsearchIngestion(BaseIngestion):
         except Exception as e:
             self.logger.error(f"Exception while quering elasticsearch: {e}")
         else:
-            self.logger.info(f"Successfully got {len(response.aggregations.login_user.buckets)} users")
-            for user in response.aggregations.login_user.buckets:
-                users_list.append(user.key)
-            return users_list
+            if response.aggregations:
+                self.logger.info(f"Successfully got {len(response.aggregations.login_user.buckets)} users")
+                for user in response.aggregations.login_user.buckets:
+                    if user.key:  # exclude not well-formatted usernames (e.g. "")
+                        users_list.append(user.key)
+        return users_list
 
-    def process_user_logins(self, start_date, end_date, username):
+    def process_user_logins(self, start_date: datetime, end_date: datetime, username: str) -> list:
+        """
+        Concrete implementation of the BaseIngestion.process_user_logins abstract method
+
+        :param username: username of the user that logged in Elasticsearch
+        :type username: str
+        :param start_date: the initial datetime from which the logins of the user are considered
+        :type start_date: datetime (with tzinfo=datetime.timezone.utc)
+        :param end_date: the final datetime within which the logins of the user are considered
+        :type end_date: datetime (with tzinfo=datetime.timezone.utc)
+
+        :return: Elasticsearch response of logins of that username
+        :rtype: elasticsearch_dsl.response.Response
+        """
         s = (
             Search(index=self.elastic_config["indexes"])
             .filter("range", **{"@timestamp": {"gte": start_date, "lt": end_date}})
             .query("match", **{"user.name": username})
+            .query("match", **{"event.category": "authentication"})
             .query("match", **{"event.outcome": "success"})
             .query("match", **{"event.type": "start"})
             .query("exists", field="source.ip")
@@ -84,30 +112,38 @@ class ElasticsearchIngestion(BaseIngestion):
             return response
 
     def normalize_fields(self, logins_response):
+        """
+        Concrete implementation of the BaseIngestion.normalize_fields abstract method
+
+        :param logins_response: user related logins returned by the Elasticsearch query
+        :type logins_response: Elasticsearch response
+
+        :return: list of normalized logins
+        :rtype: list
+        """
         fields = []
         for hit in logins_response:
             if "source" in hit:
-                tmp = {"timestamp": hit["@timestamp"]}
-                tmp["id"] = hit.meta["id"]
-                if hit.meta["index"].split("-")[0] == "fw":
-                    tmp["index"] = "fw-proxy"
-                else:
-                    tmp["index"] = hit.meta["index"].split("-")[0]
-                tmp["ip"] = hit["source"]["ip"]
-                if "user_agent" in hit:
-                    tmp["agent"] = hit["user_agent"]["original"]
-                else:
-                    tmp["agent"] = ""
-                if "as" in hit.source:
-                    tmp["organization"] = hit["source"]["as"]["organization"]["name"]
-                if "geo" in hit.source:
-                    if "location" in hit.source.geo and "country_name" in hit.source.geo:
-                        tmp["lat"] = hit["source"]["geo"]["location"]["lat"]
-                        tmp["lon"] = hit["source"]["geo"]["location"]["lon"]
-                        tmp["country"] = hit["source"]["geo"]["country_name"]
-                    else:
-                        tmp["lat"] = None
-                        tmp["lon"] = None
-                        tmp["country"] = ""
-                    fields.append(tmp)  # up to now: no geo info --> login discard
+                tmp = {
+                    "timestamp": hit["@timestamp"],
+                    "id": getattr(getattr(hit, "meta", {}), "id", ""),
+                    "index": "fw-proxy" if getattr(getattr(hit, "meta", {}), "index", "").startswith("fw-") else getattr(hit.meta, "index", "").split("-")[0],
+                    "ip": hit["source"]["ip"],
+                    "agent": getattr(getattr(hit, "user_agent", {}), "original", ""),
+                    "organization": getattr(getattr(getattr(getattr(hit, "source", {}), "as", {}), "organization", {}), "name", ""),
+                }
+
+                # geolocation fields
+                geo_dict = getattr(getattr(hit, "source", {}), "geo", {})
+                if "location" in geo_dict and "country_name" in geo_dict:
+                    # no geo info --> login discard
+                    tmp.update(
+                        {
+                            "lat": getattr(getattr(geo_dict, "location", {}), "lat", None),
+                            "lon": getattr(getattr(geo_dict, "location", {}), "lon", None),
+                            "country": getattr(getattr(geo_dict, "location", {}), "country_name", ""),
+                        }
+                    )
+
+                    fields.append(tmp)
         return fields
