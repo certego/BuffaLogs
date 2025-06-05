2d5f31977562bcca2f06e01a061f17c1a7690651
kunalsz <kunalavengers@gmail.com>
2025-06-05 12:50:41 +0530

    Adds RocketChat Alerter
    Signed-off-by: kunalsz <kunalavengers@gmail.com>

--- buffalogs/impossible_travel/alerting/base_alerting.py
+++ buffalogs/impossible_travel/alerting/base_alerting.py
@@ -18,6 +18,7 @@ class BaseAlerting(ABC):
         PUSHOVER = "pushover"
         DISCORD = "discord"
         MICROSOFTTEAMS = "microsoftteams"
+        ROCKETCHAT = "rocketchat"
 
     def __init__(self):
         super().__init__()
