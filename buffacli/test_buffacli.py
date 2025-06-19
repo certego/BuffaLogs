a29150fb6f0a00866f59af404a90ece2e250a983
Lorygold <lory.goldoni@gmail.com>
2025-06-19 11:28:36 +0200

    Added test_buffacli.py file

---
+++ buffacli/test_buffacli.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class BuffacliTestCase(unittest.TestCase):
+    """Class to tests buffa.py file"""
+
+    def setUp(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
