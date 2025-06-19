0aa34e16602f8b540cfecbf6f9e64cbc174a55b4
Lorena Goldoni <33703137+Lorygold@users.noreply.github.com>
2025-06-19 14:38:14 +0200

    Added test_buffacli.py file (#311)
    * Added test_buffacli.py file
    
    * Fixed black linter
    
    * Generalized buffacli CI test path
    
    * Fixed tests module buffacli

---
+++ buffacli/tests/test_buffa.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class BuffaTestCase(unittest.TestCase):
+    """Class to tests buffa.py file"""
+
+    def setUp(self):
+        pass
+
+    def test_main(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
