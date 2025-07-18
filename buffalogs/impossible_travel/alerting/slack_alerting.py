424ad459823b7f2cbd8c97271e7a4394fe839478
zeit <75166996+kunalsz@users.noreply.github.com>
2025-07-18 21:08:10 +0530

    Retry mechanism with exponential backoff (#348)
    * Implement retry mechanism with exponential backoff
    
    Signed-off-by: kunalsz <kunalavengers@gmail.com>
    
    * Implement backoff for exponential retrying
    
    Signed-off-by: kunalsz <kunalavengers@gmail.com>
    
    * Implement backoff for the rest of the alerters
    
    Signed-off-by: kunalsz <kunalavengers@gmail.com>
    
    ---------
    
    Signed-off-by: kunalsz <kunalavengers@gmail.com>

--- buffalogs/impossible_travel/alerting/slack_alerting.py
+++ buffalogs/impossible_travel/alerting/slack_alerting.py
@@ -2,6 +2,7 @@ try:
     import requests
 except ImportError:
     pass
+import backoff
 from django.db.models import Q
 from impossible_travel.alerting.base_alerting import BaseAlerting
 from impossible_travel.models import Alert
@@ -23,6 +24,27 @@ class SlackAlerting(BaseAlerting):
             self.logger.error("Slack Alerter configuration is missing required fields.")
             raise ValueError("Slack Alerter configuration is missing required fields.")
 
+    @backoff.on_exception(backoff.expo, requests.RequestException, max_tries=5, base=2)
+    def send_message(self, alert):
+        alert_title, alert_description = self.alert_message_formatter(alert)
+        alert_msg = {
+            "attachments": [
+                {
+                    "title": alert_title,
+                    "text": alert_description,
+                    "color": "#ff0000",  # red color
+                }
+            ]
+        }
+
+        resp = requests.post(self.webhook_url, json=alert_msg, headers={"Content-Type": "application/json"})
+        resp.raise_for_status()
+        self.logger.info(f"Slack alert sent: {alert.name}")
+        alert.notified_status["slack"] = True
+        alert.save()
+
+        return resp
+
     def notify_alerts(self):
         """
         Execute the alerter operation.
@@ -30,21 +52,7 @@ class SlackAlerting(BaseAlerting):
         alerts = Alert.objects.filter(Q(notified_status__slack=False) | ~Q(notified_status__has_key="slack"))
 
         for alert in alerts:
-            alert_title, alert_description = self.alert_message_formatter(alert)
-            alert_msg = {
-                "attachments": [
-                    {
-                        "title": alert_title,
-                        "text": alert_description,
-                        "color": "#ff0000",
-                    }
-                ]
-            }
             try:
-                resp = requests.post(self.webhook_url, json=alert_msg, headers={"Content-Type": "application/json"})
-                resp.raise_for_status()
-                self.logger.info(f"Slack alert sent: {alert.name}")
-                alert.notified_status["slack"] = True
-                alert.save()
+                self.send_message(alert)
             except requests.RequestException as e:
                 self.logger.exception(f"Slack alert failed for {alert.name}: {str(e)}")
