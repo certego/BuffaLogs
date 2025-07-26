dca98f348c931486f6e419b05f6d9e46f518a876
zeit <75166996+kunalsz@users.noreply.github.com>
2025-07-26 11:50:09 +0530

    Add `test_alert_network_failure` back to test_alert_discord.py

--- buffalogs/impossible_travel/tests/alerters/test_alert_discord.py
+++ buffalogs/impossible_travel/tests/alerters/test_alert_discord.py
@@ -65,3 +65,15 @@ class TestDiscordAlerting(TestCase):
         """Test that an error is raised if the configuration is not correct"""
         with self.assertRaises(ValueError):
             DiscordAlerting({})
+
+    @patch("requests.post")
+    def test_alert_network_failure(self, mock_post):
+        """Test that alert is not marked as notified if there are any Network Fails"""
+        # Simulate network/API failure
+        mock_post.side_effect = requests.RequestException()
+
+        self.discord_alerting.notify_alerts()
+
+        # Reload the alert from DB to check its state
+        alert = Alert.objects.get(pk=self.alert.pk)
+        self.assertFalse(alert.notified_status["discord"])
