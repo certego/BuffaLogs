9a5735f437f3018d7818a5e003640070d92a3ea3
Noble47 <noblebest56@gmail.com>
2025-07-29 14:05:06 +0100

    Feature [buffacli] Added Generic Request Exception Handling

--- buffacli/src/buffacli/requests.py
+++ buffacli/src/buffacli/requests.py
@@ -1,5 +1,6 @@
 import requests
 from buffacli.config import get_buffalogs_url
+from buffacli.exception_handlers import request_exception_handler
 
 root_url = get_buffalogs_url()
 alert_types_api = root_url / "api/alert_types"
@@ -7,29 +8,36 @@ ingestion_api = root_url / "api/ingestion"
 alerters_api = root_url / "api/alerters"
 
 
+@request_exception_handler
+def send_request(url, *args, **kwargs):
+    req = requests.get(url, *args, **kwargs)
+    req.raise_for_status()
+    return req
+
+
 def get_alert_types():
-    return requests.get(alert_types_api).json()
+    return send_request(alert_types_api).json()
 
 
 def get_active_ingestion_source():
-    return requests.get(ingestion_api / "active_ingestion_source").json()
+    return send_request(ingestion_api / "active_ingestion_source").json()
 
 
 def get_ingestion_source(source: str):
-    return requests.get(ingestion_api / source).json()
+    return send_request(ingestion_api / source).json()
 
 
 def get_ingestion_sources():
-    return requests.get(ingestion_api / "sources").json()
+    return send_request(ingestion_api / "sources").json()
 
 
 def get_alerters():
-    return requests.get(alerters_api).json()
+    return send_request(alerters_api).json()
 
 
 def get_active_alerter():
-    return requests.get(alerters_api / "active-alerter").json()
+    return send_request(alerters_api / "active-alerter").json()
 
 
 def get_alerter_config(alerter: str):
-    return requests.get(alerters_api / f"{alerter}").json()
+    return send_request(alerters_api / f"{alerter}").json()
