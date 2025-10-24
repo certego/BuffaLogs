c7b438c09081665b978ec188e933bce2a38440cc
saurabh007007 <loverboybhol143@gmail.com>
2025-10-24 13:27:26 +0530

    fix:removed extra test database from seetings.py

--- buffalogs/buffalogs/settings/settings.py
+++ buffalogs/buffalogs/settings/settings.py
@@ -141,17 +141,6 @@ DATABASES = {
         "PORT": CERTEGO_BUFFALOGS_POSTGRES_PORT,
     }
 }
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "buffalogs",
-        "USER": "postgres",
-        "PASSWORD": "your_postgres_password",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
-}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
