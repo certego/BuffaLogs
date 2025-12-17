593533913f62da662418cdf19171daa14e87d57b
Lorygold <lory.goldoni@gmail.com>
2025-12-17 18:56:39 +0100

    Added django.contrib.postgres in the INSTALLED_APPS in order to use ArrayField in models

--- buffalogs/buffalogs/settings/settings.py
+++ buffalogs/buffalogs/settings/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     "impossible_travel",
     "rest_framework",
     "rest_framework.authtoken",
